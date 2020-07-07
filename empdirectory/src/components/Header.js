f9b98c3d552e0094525f1b6772391dd419fdade2
dfuenning <dfuenning@gmail.com>
2020-07-06 21:57:39 -0400

    search function and file structure

--- empdirectory/src/components/Header.js
+++ empdirectory/src/components/Header.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "../styles/Header.css";
 
 export default class Header extends Component {
--- empdirectory/src/components/Nav.js
+++ empdirectory/src/components/Nav.js
@@ -5,17 +5,6 @@ import "../styles/Nav.css";
 function Nav({ handleSearchChange }) {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon" />
-      </button>
       <div className="collapse navbar-collapse row" id="navbarNav">
         <div className="search-area col-4">
           <SearchBox handleSearchChange={handleSearchChange}/>
